3e96571d2b6d4cb6345b61918a03a7a146c40ed2
Victor Olayemi <testimonyismine@gmail.com>
2021-05-11 10:55:46 +0100

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
-
+import { Component, ElementRef, ViewChild, OnInit, AfterViewInit } from '@angular/core';
+import * as tmImage from '@teachablemachine/image';
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+ selector: 'app-root',
+ templateUrl: './app.component.html',
+ styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'Teachable-Machine-Angular';
+export class AppComponent implements OnInit, AfterViewInit {
+ url = 'https://teachablemachine.withgoogle.com/models/r7aSS2SO';
+ model: any;
+ predictions: any;
+ webcam: any; 
+ maxPredictions: any;
+ loading: boolean = true;
+
+ @ViewChild("video", { static: true}) video: ElementRef;
+
+
+ ngOnInit(){}
+
+ async ngAfterViewInit() {
+
+  const modelURL = this.url + '/model.json';
+  const metadataURL = this.url + '/metadata.json';
+  this.model = await tmImage.load(modelURL, metadataURL);
+  this.maxPredictions = this.model.getTotalClasses();
+  // Convenience function to setup a webcam
+  const flip = true; // whether to flip the webcam 
+  this.webcam = new tmImage.Webcam(640, 480, flip);
+  await this.webcam.setup(); // request access to the webcam
+  this.loading= false;
+  await this.webcam.play();
+  requestAnimationFrame(() =>
+   this.loop()
+  );
+  this.video.nativeElement.appendChild(this.webcam.canvas);
+ }
+
+async loop() {
+ this.webcam.update(); // update the webcam frame
+ this.predictions = await this.model.predict(this.webcam.canvas,  true);
+ requestAnimationFrame(() =>
+  this.loop()
+ );
+ }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -10,7 +15,12 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatProgressBarModule,
+    MatToolbarModule,
+    FlexLayoutModule,
+    MatProgressSpinnerModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
